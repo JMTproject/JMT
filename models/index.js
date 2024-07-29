f3417b9eff2e2dcdfca802c5bd56997bd110a897
offonclass <offonclass@gmail.com>
2024-07-29 14:41:53 +0900

    모델 샘플

--- models/index.js
+++ models/index.js
@@ -8,6 +8,7 @@ const db = {};
 
 let sequelize = new Sequelize(config.database, config.username, config.password, config);
 
+db.User = require('./user')(sequelize)
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const db = require('./models');
 const app = express();
 const PORT = 8000;
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 //라우터
 const pageRouter = require('./routes/page')
 app.use('/', pageRouter)
 
+db.sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`http://localhost:${PORT}`);
+    });
+})
 
-app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
-});
