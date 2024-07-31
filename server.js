c7f62dd3ba9ead5cc6d11f74aecf0f7d0619db98
SeyongA <tlstpdyd11@naver.com>
2024-07-31 21:00:12 +0900

    로그인, 회원가입, 프로필 수정 css

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const app = express();
 const PORT = 8000;
 
 app.set('view engine', 'ejs');
+app.use('/public', express.static(__dirname + '/public'))
 
 //라우터
-const pageRouter = require('./routes/page')
-app.use('/', pageRouter)
-const userRouter = require('./routes/user')
-app.use('/api/user', userRouter)
+const pageRouter = require('./routes/page');
+app.use('/', pageRouter);
+const userRouter = require('./routes/user');
+app.use('/api/user', userRouter);
 
 db.sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
         console.log(`http://localhost:${PORT}`);
     });
-})
-
+});
