e320bcba0f9073e99a0f28bdb13da566812d83f2
offonclass <offonclass@gmail.com>
2024-08-01 15:46:29 +0900

    router 정리

---
+++ controller/recipe.js
@@ -0,0 +1,7 @@
+const {User, Recipe, Review, Ingredient, CookingTools, CookingStep} = require('../models')
+
+const recipe = (req, res) => {
+    console.log('sample');
+}
+
+module.exports = {recipe}
