93b6c7f1118ccb43d8adf055ae61e998730ee3ab
정영훈 <jung-younghun@jeong-yeonghun-ui-MacBookPro.local>
2024-08-01 15:12:10 +0900

    hun upload write/updaterecipe

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = 8000;
 
 app.set('view engine', 'ejs');
+app.use('/public', express.static(__dirname + '/public'))
 
 //라우터
 const pageRouter = require('./routes/page')
