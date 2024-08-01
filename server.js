17e8315a152308d369fcc64b47b650008150b158
segretoo <segreto8867@gamil.com>
2024-08-01 16:41:53 +0900

    recipe_detail_css

--- server.js
+++ server.js
@@ -7,14 +7,15 @@ const PORT = 8000;
 app.set('view engine', 'ejs');
 
 //라우터
-const pageRouter = require('./routes/page')
-app.use('/', pageRouter)
-const userRouter = require('./routes/user')
-app.use('/api/user', userRouter)
+const pageRouter = require('./routes/page');
+app.use('/', pageRouter);
+const userRouter = require('./routes/user');
+app.use('/api/user', userRouter);
+
+app.use(express.static('./views/css'));
 
 db.sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
         console.log(`http://localhost:${PORT}`);
     });
-})
-
+});
