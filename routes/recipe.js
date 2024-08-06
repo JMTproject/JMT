a595a23be9f5dcd6431493bc285cfc3840600125
segretoo <segreto8867@gamil.com>
2024-08-06 15:17:42 +0900

    moon_upload

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-// const Recipe = require('../models/Recipe');
-
 const { recipeList } = require('../controller/main');
 const { uploadFunc } = require('../controller/writeRecipe');
+const recipe = require('../controller/recipe');
 
 router.post('/recipelist', recipeList); //hyun
 router.post('/writerecipe', uploadFunc);
-
-// router.get('/recipe/:id', async (req, res) => {
-//     try {
-//         const recipe = await Recipe.findByPk(req.params.id);
-//         res.json(recipe);
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-// });
+router.get('/data/:id', recipe.getRecipePage);
 
 module.exports = router;
