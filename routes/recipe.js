846552d52ba9039c428f1fb37dd800deb61a6338
정영훈 <jung-younghun@jeong-yeonghun-ui-MacBookPro.local>
2024-08-11 13:23:55 +0900

    yhun -> dev

--- routes/recipe.js
+++ routes/recipe.js
@@ -7,10 +7,16 @@ const { myRecipeList, myRecipeSearch } = require('../controller/myRecipe');
 const recipe = require('../controller/recipe');
 const { verify } = require('../controller/verify');
 
+const { openRecipe } = require('../controller/openRecipe');
+const { updateFunc } = require('../controller/updateRecipe');
+
 router.post('/recipelist', recipeList); //hyun
 router.post('/writerecipe', auth, uploadFunc);
 router.get('/data/:id', recipe.getRecipePage);
 router.post('/writerecipe', uploadFunc);
+router.post('/updaterecipe', auth, updateFunc);
+
+router.get('/updaterecipe/:id', auth, openRecipe);
 router.post('/verify', auth, verify);
 router.post('/myrecipe', auth, myRecipeList); //hyun
 router.post('/mysearch', auth, myRecipeSearch); //hyun
