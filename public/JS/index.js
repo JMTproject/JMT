951d6b7221ce68c32c6ae1959453bead3e9fd70a
segretoo <segreto8867@gamil.com>
2024-08-12 20:09:18 +0900

    moon_review

--- public/JS/index.js
+++ public/JS/index.js
@@ -19,17 +19,17 @@ let endRecipe = recipesPerPage;
 
 // 레시피카드 펼지기
 const makeRecipeCard = () => {
-  currentRecipes = allRecipe.slice(startRecipe, endRecipe);
+    currentRecipes = allRecipe.slice(startRecipe, endRecipe);
 
-  recipeUl.innerHTML = '';
-  currentRecipes.forEach((recipe) => {
-    const ratingStars = parseInt(recipe.rating);
-    let starsHtml = '';
-    for (i = 1; i <= ratingStars; i++) {
-      starsHtml += '<span class="material-icons-round star">star</span>';
-    }
+    recipeUl.innerHTML = '';
+    currentRecipes.forEach((recipe) => {
+        const ratingStars = parseInt(recipe.rating);
+        let starsHtml = '';
+        for (i = 1; i <= ratingStars; i++) {
+            starsHtml += '<span class="material-symbols-rounded star">star</span>';
+        }
 
-    recipeUl.innerHTML += `
+        recipeUl.innerHTML += `
         <li id="sample">
               <a href="/recipe/${recipe.recipeId}">
                 <div id="recipeCard">
@@ -39,7 +39,7 @@ const makeRecipeCard = () => {
                   </div>
                   <div id="content">
                   <div id="viewCount">
-                    <span class="material-icons-round visivility">visibility</span>
+                    <span class="material-symbols-rounded visibility">visibility</span>
                     <p>${recipe.viewCount}</p>
                   </div>
                     <div id="title">
@@ -60,224 +60,221 @@ const makeRecipeCard = () => {
               </a>
             </li>
         `;
-  });
+    });
 };
 
 // 페이지네이션 버튼 생성하고 배열에 넣기
 const makePageButtons = () => {
-  totalPages = Math.ceil(allRecipe.length / recipesPerPage);
+    totalPages = Math.ceil(allRecipe.length / recipesPerPage);
 
-  for (i = 1; i <= totalPages; i++) {
-    // 버튼 5개씩 buttonPack에 넣기
-    if (buttonPack.length === 5) {
-      pageButtons.push(buttonPack);
-      buttonPack = [];
+    for (i = 1; i <= totalPages; i++) {
+        // 버튼 5개씩 buttonPack에 넣기
+        if (buttonPack.length === 5) {
+            pageButtons.push(buttonPack);
+            buttonPack = [];
+        }
+        buttonPack.push(`<span id="page${i}" onclick='selectPage(${i})'>${i}</span>`);
+        if (i === totalPages) {
+            pageButtons.push(buttonPack);
+            buttonPack = [];
+        }
     }
-    buttonPack.push(`<span id="page${i}" onclick='selectPage(${i})'>${i}</span>`);
-    if (i === totalPages) {
-      pageButtons.push(buttonPack);
-      buttonPack = [];
+    paginationBox.innerHTML = '';
+    currentButtonPack = 0;
+
+    pageButtons[currentButtonPack].forEach((html) => {
+        paginationBox.innerHTML += html;
+    });
+
+    if (currentButtonPack !== pageButtons.length - 1) {
+        paginationBox.innerHTML += `<span class="material-icons-round arrow_right" onclick="nextPages()">arrow_right</span>`;
     }
-  }
-  paginationBox.innerHTML = '';
-  currentButtonPack = 0;
-
-  pageButtons[currentButtonPack].forEach((html) => {
-    paginationBox.innerHTML += html;
-  });
-
-  if (currentButtonPack !== pageButtons.length - 1) {
-    paginationBox.innerHTML += `<span class="material-icons-round arrow_right" onclick="nextPages()">arrow_right</span>`;
-  }
 };
 
 // next버튼 클릭시 페이지 번호 전환
 const nextPages = () => {
-  paginationBox.innerHTML = '';
-  currentButtonPack++;
-  if (currentButtonPack !== 0) {
-    paginationBox.innerHTML += `<span class="material-icons-round arrow_left" onclick="prevPages()">arrow_left</span>`;
-  }
+    paginationBox.innerHTML = '';
+    currentButtonPack++;
+    if (currentButtonPack !== 0) {
+        paginationBox.innerHTML += `<span class="material-icons-round arrow_left" onclick="prevPages()">arrow_left</span>`;
+    }
 
-  pageButtons[currentButtonPack].forEach((html) => {
-    paginationBox.innerHTML += html;
-  });
+    pageButtons[currentButtonPack].forEach((html) => {
+        paginationBox.innerHTML += html;
+    });
 
-  if (currentButtonPack !== pageButtons.length - 1) {
-    paginationBox.innerHTML += `<span class="material-icons-round arrow_right" onclick="nextPages()">arrow_right</span>`;
-  }
+    if (currentButtonPack !== pageButtons.length - 1) {
+        paginationBox.innerHTML += `<span class="material-icons-round arrow_right" onclick="nextPages()">arrow_right</span>`;
+    }
 };
 
 // prev버튼 클릭시 페이지 번호 전환
 const prevPages = () => {
-  paginationBox.innerHTML = '';
-  currentButtonPack--;
-  if (currentButtonPack !== 0) {
-    paginationBox.innerHTML += `<span class="material-icons-round arrow_left" onclick="prevPages()">arrow_left</span>`;
-  }
+    paginationBox.innerHTML = '';
+    currentButtonPack--;
+    if (currentButtonPack !== 0) {
+        paginationBox.innerHTML += `<span class="material-icons-round arrow_left" onclick="prevPages()">arrow_left</span>`;
+    }
 
-  pageButtons[currentButtonPack].forEach((html) => {
-    paginationBox.innerHTML += html;
-  });
+    pageButtons[currentButtonPack].forEach((html) => {
+        paginationBox.innerHTML += html;
+    });
 
-  if (currentButtonPack !== pageButtons.length - 1) {
-    paginationBox.innerHTML += `<span class="material-icons-round arrow_right" onclick="nextPages()">arrow_right</span>`;
-  }
+    if (currentButtonPack !== pageButtons.length - 1) {
+        paginationBox.innerHTML += `<span class="material-icons-round arrow_right" onclick="nextPages()">arrow_right</span>`;
+    }
 };
 
 // 페이지 번호 클릭시 레시피 펼치기
 const selectPage = (page) => {
-  const pageButton = document.querySelector(`#page${page}`);
-  const allPageButton = document.querySelectorAll('#paginationBox span');
+    const pageButton = document.querySelector(`#page${page}`);
+    const allPageButton = document.querySelectorAll('#paginationBox span');
 
-  allPageButton.forEach((button) => {
-    button.style.backgroundColor = '#fff';
-  });
+    allPageButton.forEach((button) => {
+        button.style.backgroundColor = '#fff';
+    });
 
-  pageButton.style.backgroundColor = '#FCA391';
+    pageButton.style.backgroundColor = '#FCA391';
 
-  endRecipe = page * recipesPerPage;
-  startRecipe = endRecipe - recipesPerPage;
-  makeRecipeCard();
+    endRecipe = page * recipesPerPage;
+    startRecipe = endRecipe - recipesPerPage;
+    makeRecipeCard();
 };
-
-
 
 // 새로고침
 (async function recipeList() {
-  orderByLatestButton.style.backgroundColor = '#fff';
-  orderByStarsButton.style.backgroundColor = '#FCA391';
-  orderByViewCountButton.style.backgroundColor = '#fff';
-  recipeUl.innerHTML = '';
-  const res = await axios({
-    method: 'post',
-    url: '/api/recipe/recipelist',
-  });
-  allRecipe = res.data.allRecipe;
+    orderByLatestButton.style.backgroundColor = '#fff';
+    orderByStarsButton.style.backgroundColor = '#FCA391';
+    orderByViewCountButton.style.backgroundColor = '#fff';
+    recipeUl.innerHTML = '';
+    const res = await axios({
+        method: 'post',
+        url: '/api/recipe/recipelist',
+    });
+    allRecipe = res.data.allRecipe;
 
-  // 모든 레시피 별점 평균 계산
-  const C = allRecipe.reduce((acc, recipe) => acc + parseFloat(recipe.rating), 0) / allRecipe.length;
-  // m: 리뷰수의 중요도를 결정하는 계수 -> 사용자가 많아 전체적인 리뷰수가 높을수록 높게 잡아야함
-  const m = 10;
-  // 가중 평균 계산하여 allRecipe에 속성 추가
-  allRecipe.forEach((recipe) => {
-    recipe.wr = (recipe.reviewCount * parseFloat(recipe.rating) + C * m) / (parseInt(recipe.reviewCount) + m);
-  });
-  // wr값을 기준으로 내림차순으로 정렬
-  allRecipe.sort((a, b) => b.wr - a.wr);
+    // 모든 레시피 별점 평균 계산
+    const C = allRecipe.reduce((acc, recipe) => acc + parseFloat(recipe.rating), 0) / allRecipe.length;
+    // m: 리뷰수의 중요도를 결정하는 계수 -> 사용자가 많아 전체적인 리뷰수가 높을수록 높게 잡아야함
+    const m = 10;
+    // 가중 평균 계산하여 allRecipe에 속성 추가
+    allRecipe.forEach((recipe) => {
+        recipe.wr = (recipe.reviewCount * parseFloat(recipe.rating) + C * m) / (parseInt(recipe.reviewCount) + m);
+    });
+    // wr값을 기준으로 내림차순으로 정렬
+    allRecipe.sort((a, b) => b.wr - a.wr);
 
-  startRecipe = 0;
-  endRecipe = recipesPerPage;
-  makeRecipeCard();
-  // 페이지네이션
-  makePageButtons();
+    startRecipe = 0;
+    endRecipe = recipesPerPage;
+    makeRecipeCard();
+    // 페이지네이션
+    makePageButtons();
 })();
 
 const orderByLatest = async () => {
-  orderByLatestButton.style.backgroundColor = '#FCA391';
-  orderByStarsButton.style.backgroundColor = '#fff';
-  orderByViewCountButton.style.backgroundColor = '#fff';
-  recipeUl.innerHTML = '';
-  const res = await axios({
-    method: 'post',
-    url: '/api/recipe/recipelist',
-  });
-  allRecipe = res.data.allRecipe;
-  // console.log('allRecipe', allRecipe);
+    orderByLatestButton.style.backgroundColor = '#FCA391';
+    orderByStarsButton.style.backgroundColor = '#fff';
+    orderByViewCountButton.style.backgroundColor = '#fff';
+    recipeUl.innerHTML = '';
+    const res = await axios({
+        method: 'post',
+        url: '/api/recipe/recipelist',
+    });
+    allRecipe = res.data.allRecipe;
+    // console.log('allRecipe', allRecipe);
 
-  startRecipe = 0;
-  endRecipe = recipesPerPage;
-  makeRecipeCard();
-  // 페이지네이션
-  makePageButtons();
+    startRecipe = 0;
+    endRecipe = recipesPerPage;
+    makeRecipeCard();
+    // 페이지네이션
+    makePageButtons();
 };
 
 const orderByStars = async () => {
-  orderByLatestButton.style.backgroundColor = '#fff';
-  orderByStarsButton.style.backgroundColor = '#FCA391';
-  orderByViewCountButton.style.backgroundColor = '#fff';
-  recipeUl.innerHTML = '';
-  const res = await axios({
-    method: 'post',
-    url: '/api/recipe/recipelist',
-  });
-  allRecipe = res.data.allRecipe;
+    orderByLatestButton.style.backgroundColor = '#fff';
+    orderByStarsButton.style.backgroundColor = '#FCA391';
+    orderByViewCountButton.style.backgroundColor = '#fff';
+    recipeUl.innerHTML = '';
+    const res = await axios({
+        method: 'post',
+        url: '/api/recipe/recipelist',
+    });
+    allRecipe = res.data.allRecipe;
 
-  // 모든 레시피 별점 평균 계산
-  const C = allRecipe.reduce((acc, recipe) => acc + parseFloat(recipe.rating), 0) / allRecipe.length;
+    // 모든 레시피 별점 평균 계산
+    const C = allRecipe.reduce((acc, recipe) => acc + parseFloat(recipe.rating), 0) / allRecipe.length;
 
-  // m: 리뷰수의 중요도를 결정하는 계수 -> 사용자가 많아 전체적인 리뷰수가 높을수록 높게 잡아야함
-  const m = 10;
+    // m: 리뷰수의 중요도를 결정하는 계수 -> 사용자가 많아 전체적인 리뷰수가 높을수록 높게 잡아야함
+    const m = 10;
 
-  // 가중 평균 계산하여 allRecipe에 속성 추가
-  allRecipe.forEach((recipe) => {
-    recipe.wr = (recipe.reviewCount * parseFloat(recipe.rating) + C * m) / (parseInt(recipe.reviewCount) + m);
-  });
-  console.log('콘솔확인@@@', allRecipe);
+    // 가중 평균 계산하여 allRecipe에 속성 추가
+    allRecipe.forEach((recipe) => {
+        recipe.wr = (recipe.reviewCount * parseFloat(recipe.rating) + C * m) / (parseInt(recipe.reviewCount) + m);
+    });
+    console.log('콘솔확인@@@', allRecipe);
 
-  // wr값을 기준으로 내림차순으로 정렬
-  allRecipe.sort((a, b) => b.wr - a.wr);
+    // wr값을 기준으로 내림차순으로 정렬
+    allRecipe.sort((a, b) => b.wr - a.wr);
 
-  startRecipe = 0;
-  endRecipe = recipesPerPage;
-  makeRecipeCard();
-  // 페이지네이션
-  makePageButtons();
+    startRecipe = 0;
+    endRecipe = recipesPerPage;
+    makeRecipeCard();
+    // 페이지네이션
+    makePageButtons();
 };
 
 const orderByViewCount = async () => {
-  orderByLatestButton.style.backgroundColor = '#fff';
-  orderByStarsButton.style.backgroundColor = '#fff';
-  orderByViewCountButton.style.backgroundColor = '#FCA391';
-  recipeUl.innerHTML = '';
-  const res = await axios({
-    method: 'post',
-    url: '/api/recipe/recipelist',
-  });
-  allRecipe = res.data.allRecipe;
+    orderByLatestButton.style.backgroundColor = '#fff';
+    orderByStarsButton.style.backgroundColor = '#fff';
+    orderByViewCountButton.style.backgroundColor = '#FCA391';
+    recipeUl.innerHTML = '';
+    const res = await axios({
+        method: 'post',
+        url: '/api/recipe/recipelist',
+    });
+    allRecipe = res.data.allRecipe;
 
-  // viewCount값을 기준으로 내림차순으로 정렬
-  allRecipe.sort((a, b) => b.viewCount - a.viewCount);
+    // viewCount값을 기준으로 내림차순으로 정렬
+    allRecipe.sort((a, b) => b.viewCount - a.viewCount);
 
-  startRecipe = 0;
-  endRecipe = recipesPerPage;
-  makeRecipeCard();
-  // 페이지네이션
-  makePageButtons();
+    startRecipe = 0;
+    endRecipe = recipesPerPage;
+    makeRecipeCard();
+    // 페이지네이션
+    makePageButtons();
 };
-
 
 // 검색된 레시피 나열
 const enterkeySearch = async () => {
-  const keyword = document.querySelector('#searchInput').value;
+    const keyword = document.querySelector('#searchInput').value;
 
-  const data = { keyword };
+    const data = { keyword };
 
-  const res = await axios({
-    method: 'post',
-    url: '/api/recipe/search',
-    data,
-  });
-  console.log('검색된 모든 레시피' ,res.data.allRecipe);
+    const res = await axios({
+        method: 'post',
+        url: '/api/recipe/search',
+        data,
+    });
+    console.log('검색된 모든 레시피', res.data.allRecipe);
 
-  allRecipe = res.data.allRecipe;
+    allRecipe = res.data.allRecipe;
 
-  // 별점순 조건문
-  if(orderByStarsButton.style.backgroundColor === '#FCA391') {
-  const C = allRecipe.reduce((acc, recipe) => acc + parseFloat(recipe.rating), 0) / allRecipe.length;
-  const m = 10;
-  allRecipe.forEach((recipe) => {
-    recipe.wr = (recipe.reviewCount * parseFloat(recipe.rating) + C * m) / (parseInt(recipe.reviewCount) + m);
-  });
-  allRecipe.sort((a, b) => b.wr - a.wr);
-  // 조회수순 조건문
-  } else if(orderByViewCountButton.style.backgroundColor === '#FCA391' ) {
-    allRecipe.sort((a, b) => b.viewCount - a.viewCount);
-  }
+    // 별점순 조건문
+    if (orderByStarsButton.style.backgroundColor === '#FCA391') {
+        const C = allRecipe.reduce((acc, recipe) => acc + parseFloat(recipe.rating), 0) / allRecipe.length;
+        const m = 10;
+        allRecipe.forEach((recipe) => {
+            recipe.wr = (recipe.reviewCount * parseFloat(recipe.rating) + C * m) / (parseInt(recipe.reviewCount) + m);
+        });
+        allRecipe.sort((a, b) => b.wr - a.wr);
+        // 조회수순 조건문
+    } else if (orderByViewCountButton.style.backgroundColor === '#FCA391') {
+        allRecipe.sort((a, b) => b.viewCount - a.viewCount);
+    }
 
-  startRecipe = 0;
-  endRecipe = recipesPerPage;
-  makeRecipeCard();
-  // 페이지네이션
-  makePageButtons();
+    startRecipe = 0;
+    endRecipe = recipesPerPage;
+    makeRecipeCard();
+    // 페이지네이션
+    makePageButtons();
 };
