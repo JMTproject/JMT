506cb375905803f3a18825f2504fa370f537a5c9
정영훈 <jung-younghun@jeong-yeonghun-ui-MacBookPro.local>
2024-08-13 19:21:18 +0900

    huny -> dev

--- models/cookingTools.js
+++ models/cookingTools.js
@@ -1,29 +1,25 @@
 const { DataTypes } = require('sequelize');
 
 const cookingTools = (seq) => {
-    return seq.define('cookingTools', {
-        toolId : {
-            type : DataTypes.INTEGER,
-            allowNull : false,
-            autoIncrement : true,
-            primaryKey : true,
-        },
-        toolNum : {
-            type : DataTypes.INTEGER(2),
-            allowNull : false,
-        },
-        toolName : {
-            type : DataTypes.STRING(91),
-            allowNull : false,
-        },
-        recipeId : {
-            type : DataTypes.INTEGER,
-            references :{
-                model: 'recipes',
-                key: 'recipeId',
-            }
-        }
-    })
-}
+  return seq.define('cookingTools', {
+    toolId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    toolName: {
+      type: DataTypes.STRING(91),
+      allowNull: false,
+    },
+    recipeId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'recipes',
+        key: 'recipeId',
+      },
+    },
+  });
+};
 
 module.exports = cookingTools;
--- models/ingredient.js
+++ models/ingredient.js
@@ -1,33 +1,29 @@
 const { DataTypes } = require('sequelize');
 
 const ingredient = (seq) => {
-    return seq.define('ingredient', {
-        ingredientId : {
-            type : DataTypes.INTEGER,
-            allowNull : false,
-            autoIncrement : true,
-            primaryKey : true,
-        },
-        ingredientNum :{
-            type : DataTypes.INTEGER(2),
-            allowNull : false,
-        },
-        ingredientName : {
-            type : DataTypes.STRING(91),
-            allowNull : false,
-        },
-        quantity : {
-            type : DataTypes.STRING(31),
-            allowNull : false,
-        },
-        recipeId : {
-            type : DataTypes.INTEGER,
-            references :{
-                model: 'recipes',
-                key: 'recipeId',
-            }
-        }
-    })
-}
+  return seq.define('ingredient', {
+    ingredientId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    ingredientName: {
+      type: DataTypes.STRING(91),
+      allowNull: false,
+    },
+    quantity: {
+      type: DataTypes.STRING(31),
+      allowNull: false,
+    },
+    recipeId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'recipes',
+        key: 'recipeId',
+      },
+    },
+  });
+};
 
 module.exports = ingredient;
